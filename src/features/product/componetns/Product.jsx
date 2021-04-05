bb1b62465befd2239d5f42b7b430d23ba75650d7
ducviet2504 <ducviet2504@gmail.com>
2021-04-06 01:16:31 +0700

    add sort product

--- src/features/product/componetns/Product.jsx
+++ src/features/product/componetns/Product.jsx
@@ -12,7 +12,7 @@ Product.propTypes = {
 function Product({ product }) {
   const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
   return (
-    <Box padding={1}>
+    <Box padding={1} minHeight="215px">
       <img src={thumbnailUrl} alt={product.name} width="100%" />
 
       <Typography variant="body2">{product.name}</Typography>
