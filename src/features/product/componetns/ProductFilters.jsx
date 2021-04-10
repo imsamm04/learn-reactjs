922c594abf1528c93e447fb81db9dad989f245a0
ducviet2504 <ducviet2504@gmail.com>
2021-04-10 23:08:34 +0700

    coding filters

--- src/features/product/componetns/ProductFilters.jsx
+++ src/features/product/componetns/ProductFilters.jsx
@@ -3,29 +3,34 @@ import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core';
 import FilterByCategory from './Filters/FilterByCategory';
 import FilterByPrice from './Filters/FilterByPrice';
+import FilterByService from './Filters/FilterByService';
 
 ProductFilters.propTypes = {
     filters: PropTypes.object.isRequired,
     onChange: PropTypes.func,
 };
 
-function ProductFilters(filters, onChange) {
+function ProductFilters({filters, onChange}) {
 
     const handleCategoryChange = (newCategoryId) => {
         if(!onChange) return;
         
         const newFilters = {
-            ...filters,
             'category.id': newCategoryId,
         };
-
         onChange(newFilters);
+    }
+
+    const handleChange = (values) => {
+        if (onChange) onChange(values);
     }
 
     return (
         <div>
             <Box>
-                <FilterByCategory onChange = {handleCategoryChange} />
+                <FilterByCategory onChange = {handleChange} />
+                <FilterByPrice onChange = {handleChange} />
+                <FilterByService filters={filters} onChange = {handleChange} />
             </Box>
         </div>
     );
