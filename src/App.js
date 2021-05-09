cabf2c3000d1ea6e0ff4a5081e0032d31165deb4
ducviet2504 <ducviet2504@gmail.com>
2021-05-09 08:18:37 +0700

    add feature cart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AlbumFeature from './features/Album';
 import CounterFeature from './features/Counter';
 import ProductFeature from './features/product';
 import TodoFeature from './features/Todo';
+import CartFeature from './features/Cart';
 function App() {
   return (
     <div className="app">
@@ -20,6 +21,8 @@ function App() {
         <Route path="/todos" component={TodoFeature} />
         <Route path="/albums" component={AlbumFeature} />
         <Route path="/products" component={ProductFeature} />
+        <Route path="/cart" component={CartFeature} />
+
         <Route component={NotFound} />
       </Switch>
     </div>
